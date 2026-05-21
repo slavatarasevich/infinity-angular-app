6ff3ba8f45fcf09129f3ac03147644c7ce46d11e
Your Name <blackgrif@gmail.com>
2026-05-21 19:27:50 +0300

    first commit - stylizing login page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterOutlet],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {}
