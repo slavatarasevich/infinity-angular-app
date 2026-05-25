423c43a4f122778b0bdf4cbd03c28588d251a416
Your Name <blackgrif@gmail.com>
2026-05-25 13:20:17 +0300

    added functionality to form on Login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login implements OnInit {
+  myForm = new FormGroup({
+    login: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  logIn() {
+    console.log('TEST');
+    console.log(this.myForm.status);
+    console.log(this.myForm.value);
+
+    // console.log(this.myForm.value);
+  }
+
+  ngOnInit(): void {}
+}
